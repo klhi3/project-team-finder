8755d07772df7b1edb631cea0524dc868a0c9760
Yilun Xiao <yilunxiao2@gmail.com>
2021-05-26 11:19:39 -0400

    merged changes from main

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,6 @@
 const router = require('express').Router();
-<<<<<<< HEAD
 // const loginRoute = require('./login-route');
 
 // router.use('/login', loginRoute);
 
 module.exports = router;
-=======
-
-const userRoutes = require('./user-routes');
-
-router.use('/users', userRoutes);
-
-module.exports = router;
->>>>>>> main
