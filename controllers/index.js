4bdf7cb996ce6dcfaef9e17655f19a0df661cb1c
Yilun Xiao <yilunxiao2@gmail.com>
2021-05-26 13:05:28 -0400

    added code to project and user routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const mainRoute = require('./main-route');
+const userRoutes = require('./user-routes');
+const projectRoutes = require('./project-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', mainRoute);
+router.use('/user', userRoutes);
+router.use('/project', projectRoutes);
 
 module.exports = router;
