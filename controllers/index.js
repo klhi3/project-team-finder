957e97a84de4e979cf60927ed1cb3b283a653075
Yilun Xiao <yilunxiao2@gmail.com>
2021-05-28 10:35:32 -0400

    added skills page to check github api

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,13 @@ const mainRoute = require('./main-route');
 const userRoutes = require('./user-routes');
 const projectRoutes = require('./project-routes');
 const dashRoute = require('./dashboard-route');
+const skillRoute = require('./skillcheck-route');
 
 router.use('/api', apiRoutes);
 router.use('/', mainRoute);
 router.use('/user', userRoutes);
 router.use('/project', projectRoutes);
 router.use('/dashboard', dashRoute);
+router.use('/skill', skillRoute);
 
 module.exports = router;
---
+++ controllers/skillcheck-route.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Project, User } = require('../models');
+
+// Render Homepage
+router.get('/', async (req, res) => {
+  try {
+    // RENDERS HANDLEBAR VIEWS
+    // enter handlebars view file for users here
+    res.render('skillcheck');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
